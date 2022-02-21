bb4547e84cdb7f90d98d326715cb103f7a6db077
Jonathan Zong <jzong@mit.edu>
2022-02-21 15:00:53 -0500

    edge case handling for interpolateCatmullRom

--- packages/vega-functions/src/functions/interpolate.js
+++ packages/vega-functions/src/functions/interpolate.js
@@ -3,5 +3,6 @@ import {
 } from 'd3-interpolate-curve';
 
 export function interpolateCatmullRom(array, fraction) {
+  if (array.length === 1) return array[0];
   return d3InterpolateCatmullRom(array)(fraction);
 }
