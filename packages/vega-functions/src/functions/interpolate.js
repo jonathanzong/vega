f9806847b7b212fde3bf1a8765e9d0be08b4294f
Jonathan Zong <jzong@mit.edu>
2022-03-09 15:36:16 -0500

    memoize

--- packages/vega-functions/src/functions/interpolate.js
+++ packages/vega-functions/src/functions/interpolate.js
@@ -3,13 +3,26 @@ import {
   interpolateFromCurve,
 } from 'd3-interpolate-curve';
 import {curveLinear} from 'd3-shape';
+import { memoize } from 'lodash';
 
 export function interpolateCatmullRom(array, fraction) {
   if (array.length === 1) return array[0];
-  return d3InterpolateCatmullRom(array)(fraction);
+  return memoInterpolateCatmullRom(array)(fraction);
 }
 
 export function interpolateLinear(array, fraction) {
   if (array.length === 1) return array[0];
-  return interpolateFromCurve(array, curveLinear)(fraction);
+  return memoInterpolateLinear(array)(fraction);
 }
+
+const arrayResolver = (array) => {
+  return JSON.stringify(array);
+}
+
+const memoInterpolateLinear = memoize((array) => {
+  return interpolateFromCurve(array, curveLinear);
+}, arrayResolver);
+
+const memoInterpolateCatmullRom = memoize((array) => {
+  return d3InterpolateCatmullRom(array);
+}, arrayResolver);
